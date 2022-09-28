10df92c58d4c92e3ef240e17ad414d8ada018cf4
Cancersaintmichael <113973270+Cancersaintmichael@users.noreply.github.com>
2022-09-28 16:34:08 +0200

    3 files modified

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-console.log('Hello!');
+const openSelector = document.querySelector('.product-literal-size');
+const sizeSelector = document.querySelector('.product-size-selector');
+
+openSelector.addEventListener('click', () => {
+  sizeSelector.classList.toggle('is-visible');
+});
